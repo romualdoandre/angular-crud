45d04e753d2915181535fd3f7181516c9151acda
racosta <Romualdo.Costa@fazenda.rj.gov.br>
2022-10-07 17:35:16 -0300

    leitura de produtos e tabela

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +10,12 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from  '@angular/common/http';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatCardModule } from "@angular/material/card";
@@ -18,7 +24,11 @@ import { MatButtonModule } from  '@angular/material/button';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/products/product-create/product-create.component';
+import { ProductReadComponent } from './components/products/product-read/product-read.component';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,7 +37,8 @@ import { ProductCreateComponent } from './components/products/product-create/pro
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
@@ -39,9 +50,18 @@ import { ProductCreateComponent } from './components/products/product-create/pro
 	MatCardModule,
 	MatButtonModule,
 	MatSnackBarModule,
-	HttpClientModule
+	HttpClientModule,
+	FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+	MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
